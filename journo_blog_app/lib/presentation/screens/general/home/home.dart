d755cbeaf173d21d34a8c0ba593df390eac5ca26
Tilak Bista <155147435+TilakBistaChhetri@users.noreply.github.com>
2025-04-10 16:56:51 +0545

    next day of project

--- journo_blog_app/lib/presentation/screens/general/home/home.dart
+++ journo_blog_app/lib/presentation/screens/general/home/home.dart
@@ -79,7 +79,7 @@ class _HomeState extends State<Home> {
                             ],
                           ),
                         ],
-                      ).expand(),
+                      ).expand(),frdcviohj
                     ],
                   );
                 },
